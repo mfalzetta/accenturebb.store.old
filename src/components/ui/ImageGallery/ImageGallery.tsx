89540e7fd337a486e61eb7262dde9d83f45ffa04
joseMeneghetti <jrmp.dev@gmail.com>
2022-07-28 16:51:50 -0300

    images zoom

--- src/components/ui/ImageGallery/ImageGallery.tsx
+++ src/components/ui/ImageGallery/ImageGallery.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { Image } from 'src/components/ui/Image'
+import ImageViewer from 'react-simple-image-viewer'
 
 import { ImageGallerySelector, ImageZoom } from '.'
 import styles from './image-gallery.module.scss'
@@ -17,6 +18,24 @@ function ImageGallery({ images }: ImageGalleryProps) {
   const [selectedImageIdx, setSelectedImageIdx] = useState(0)
   const currentImage = images[selectedImageIdx]
   const hasSelector = images.length > 1
+  const [currentImageZoom, setCurrentImageZoom] = useState(0)
+  const [isViewerOpen, setIsViewerOpen] = useState(false)
+
+  const openImageViewer = useCallback((index: number) => {
+    setCurrentImageZoom(index)
+    setIsViewerOpen(true)
+  }, [])
+
+  const closeImageViewer = () => {
+    setCurrentImageZoom(0)
+    setIsViewerOpen(false)
+  }
+
+  const photos = images
+    .map((image) => {
+      return [image.url]
+    })
+    .flat()
 
   return (
     <section
@@ -32,6 +51,7 @@ function ImageGallery({ images }: ImageGalleryProps) {
           height={804 * (3 / 4)}
           loading="eager"
           fetchPriority="high"
+          onClick={() => openImageViewer(currentImageZoom)}
         />
       </ImageZoom>
       {hasSelector && (
@@ -39,6 +59,15 @@ function ImageGallery({ images }: ImageGalleryProps) {
           images={images}
           currentImageIdx={selectedImageIdx}
           onSelect={setSelectedImageIdx}
+        />
+      )}
+      {isViewerOpen && (
+        <ImageViewer
+          src={photos}
+          currentIndex={selectedImageIdx}
+          disableScroll={false}
+          closeOnClickOutside
+          onClose={closeImageViewer}
         />
       )}
     </section>
