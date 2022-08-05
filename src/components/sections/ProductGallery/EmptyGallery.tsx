f339d7738399ab14ef3b2a55f165f1290dc902e2
Samara <samara.aline.shimizu@accenture.com>
2022-08-05 17:04:53 -0300

    search not found

--- src/components/sections/ProductGallery/EmptyGallery.tsx
+++ src/components/sections/ProductGallery/EmptyGallery.tsx
@@ -1,36 +1,24 @@
-import { ButtonLink } from 'src/components/ui/Button'
 import EmptyState from 'src/components/ui/EmptyState'
-import Icon from 'src/components/ui/Icon'
+import { Icon as UIIcon } from '@faststore/ui'
+import { SearchNotFound } from 'src/images/search'
 
 function EmptyGallery() {
   return (
     <EmptyState variant="rounded">
       <header data-fs-empty-state-title>
-        <Icon name="CircleWavyWarning" width={56} height={56} weight="thin" />
-
-        <p>Nothing matches with your search</p>
+        <p data-fs-empty-state-title-text>
+          Puxa, não encontramos a palavra buscada.
+        </p>
+        <UIIcon component={<SearchNotFound />} />
       </header>
-
-      <ButtonLink
-        data-fs-empty-state-link
-        href="/office"
-        variant="secondary"
-        icon={
-          <Icon name="CircleWavyWarning" width={18} height={18} weight="bold" />
-        }
-        iconPosition="left"
-      >
-        Browse Offers
-      </ButtonLink>
-      <ButtonLink
-        data-fs-empty-state-link
-        href="/technology"
-        variant="secondary"
-        icon={<Icon name="RocketLaunch" width={18} height={18} weight="bold" />}
-        iconPosition="left"
-      >
-        Just Arrived
-      </ButtonLink>
+      <div>
+        <p data-fs-empty-state-subtitle>Quem sabe tente o seguinte:</p>
+      </div>
+      <ul data-fs-empty-state-item>
+        <li>- use palavras mais comuns</li>
+        <li>- tente termos menos específicos</li>
+        <li>- para facilitar, digite palavras de até 6 caracteres</li>
+      </ul>
     </EmptyState>
   )
 }
