1d2a5d6dcd8d978e0a2cca54258f8ce9475d2815
joseMeneghetti <jrmp.dev@gmail.com>
2022-09-16 16:32:01 -0300

    eslint disable

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@
  * */
 const nextConfig = {
   /* config options here */
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['accenturebb.vtexassets.com'],
   },
