561d6a7694d942a4e167a9b0fb3611f0ff134ce7
joseMeneghetti <jrmp.dev@gmail.com>
2022-09-20 18:48:15 -0300

    cmsPage HOOK

---
+++ src/pages/api/getCmsPage/[name].tsx
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import config from '../../../../store.config'
+
+// POST /api/user
+// Required fields in body: name, email
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const name = req?.query?.name
+  const { workspace, storeId } = config.api
+
+  try {
+    fetch(
+      `https://${workspace}--${storeId}.myvtex.com/_v/cms/api/faststore/${name}?page=1&per_page=10`
+    )
+      .then((response) => response.json())
+      .then((data) => res.status(200).json(data.data))
+  } catch (error) {
+    res.status(400).json(`Not Found, ${error}`)
+  }
+}
