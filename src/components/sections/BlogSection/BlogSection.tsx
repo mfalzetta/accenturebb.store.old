f7990e3c68aa05948cb3629fa3a33c9e5dd9cbd7
joseMeneghetti <jrmp.dev@gmail.com>
2022-09-22 11:17:36 -0300

    slider on homePage

--- src/components/sections/BlogSection/BlogSection.tsx
+++ src/components/sections/BlogSection/BlogSection.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import Link from 'src/components/ui/Link'
 import Slider from 'src/components/custom-components/home/Slider'
 
+import Section from '../Section'
+
 export interface BlogSectionAllItems {
   allItems: BlogSectionProps[]
 }
@@ -20,30 +22,36 @@ export interface BlogSectionProps {
 
 const BlogSection = ({ allItems }: BlogSectionAllItems) => {
   return (
-    <Slider minWidth={424} itemsPerPage={3}>
-      {allItems.map((card: BlogSectionProps, index: number) => (
-        <div className="blogSection__container" key={index}>
-          <Image
-            src={card.src}
-            alt={card.alt}
-            loading="lazy"
-            width={parseInt(card.width, 10)}
-            height={parseInt(card.height, 10)}
-          />
-          {card.primaryText && (
-            <p className="blogSection__primaryText">{card.primaryText}</p>
-          )}
-          {card.secondaryText && (
-            <p className="blogSection__secundaryText">{card.secondaryText}</p>
-          )}
-          {card.btnText && card.btnHref && (
-            <Link as="a" href={card.btnHref} target="_blank">
-              <Button className="blogSection__button">{card.btnText}</Button>
-            </Link>
-          )}
-        </div>
-      ))}
-    </Slider>
+    <Section
+      style={{
+        height: `${Number(allItems[0]?.height) + 150}px`,
+      }}
+    >
+      <Slider minWidth={424} itemsPerPage={3}>
+        {allItems.map((card: BlogSectionProps, index: number) => (
+          <div className="blogSection__container" key={index}>
+            <Image
+              src={card.src}
+              alt={card.alt}
+              loading="lazy"
+              width={parseInt(card.width, 10)}
+              height={parseInt(card.height, 10)}
+            />
+            {card.primaryText && (
+              <p className="blogSection__primaryText">{card.primaryText}</p>
+            )}
+            {card.secondaryText && (
+              <p className="blogSection__secundaryText">{card.secondaryText}</p>
+            )}
+            {card.btnText && card.btnHref && (
+              <Link as="a" href={card.btnHref} target="_blank">
+                <Button className="blogSection__button">{card.btnText}</Button>
+              </Link>
+            )}
+          </div>
+        ))}
+      </Slider>
+    </Section>
   )
 }
 
