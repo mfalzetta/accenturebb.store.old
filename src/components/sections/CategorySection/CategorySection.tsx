2e4d5c55d159044bce206ac285d2e771c3f85072
joseMeneghetti <jrmp.dev@gmail.com>
2022-10-13 13:34:09 -0300

    test noslider

--- src/components/sections/CategorySection/CategorySection.tsx
+++ src/components/sections/CategorySection/CategorySection.tsx
@@ -1,6 +1,9 @@
+import { useRef } from 'react'
+
 import InfoCard from 'src/components/custom-components/home/InfoCard'
-import KeenSlider from 'src/components/custom-components/KeenSlider/KeenSlider'
+// import KeenSlider from 'src/components/custom-components/KeenSlider/KeenSlider'
 import Section from 'src/components/sections/Section'
+// import useDeviceType from 'src/data/hook/useDeviceTyps'
 
 export interface CategorySectionAllItems {
   allItems: CategorySectionProps[]
@@ -15,31 +18,38 @@ export interface CategorySectionProps {
 }
 
 const CategorySection = ({ allItems }: CategorySectionAllItems) => {
+  const refContainer = useRef(null)
+
   return (
     <Section
+      ref={refContainer}
       style={{
-        height: `${Number(allItems[0]?.height) + 80}px`,
+        height: 'auto',
       }}
     >
-      <div className="layout__content classSection__container category-session">
-        <KeenSlider dots breakpoints={{ desktop: 5, tablet: 3, phone: 1 }}>
-          {allItems.map((card: CategorySectionProps, index: number) => (
-            <div
-              className={`keen-slider__slide number-slide${index}`}
-              style={{ textAlign: '-webkit-center' }}
-              key={index}
-            >
-              <InfoCard
-                href={card.href}
-                src={card.src}
-                alt={card.alt ?? `${card.text} image`}
-                width={card.width}
-                height={card.height}
-                text={card.text}
-              />
-            </div>
-          ))}
-        </KeenSlider>
+      <div
+        className="layout__content classSection__container category-session"
+        data-fs-category-section
+      >
+        {/* <KeenSlider dots breakpoints={{ desktop: 5, tablet: 3, phone: 1 }}> */}
+        {allItems.map((card: CategorySectionProps, index: number) => (
+          // <div
+          //   className={`keen-slider__slide number-slide${index}`}
+          //   style={{ textAlign: '-webkit-center' }}
+          //   key={index}
+          // >
+          <InfoCard
+            key={index}
+            href={card.href}
+            src={card.src}
+            alt={card.alt ?? `${card.text} image`}
+            width={card.width}
+            height={card.height}
+            text={card.text}
+          />
+          // </div>
+        ))}
+        {/* </KeenSlider> */}
       </div>
     </Section>
   )
