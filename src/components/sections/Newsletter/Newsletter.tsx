fec92cb23723ca84d26c59b41c5a0dcb95660f99
Samara <samara.aline.shimizu@accenture.com>
2022-10-18 11:44:44 -0300

    update

--- src/components/sections/Newsletter/Newsletter.tsx
+++ src/components/sections/Newsletter/Newsletter.tsx
@@ -1,9 +1,16 @@
 import type { ComponentPropsWithRef, FormEvent, ReactNode } from 'react'
-import { useState, forwardRef, useRef } from 'react'
-import { Form, Input, LoadingButton } from '@faststore/ui'
+import { forwardRef, useRef } from 'react'
+import { Form } from '@faststore/ui'
+
+import { useUI } from 'src/sdk/ui/Provider'
+import Icon from 'src/components/ui/Icon'
+import Button from 'src/components/ui/Button'
+import Link from 'src/components/ui/Link'
+import InputText from 'src/components/ui/InputText'
+import { useNewsletter } from 'src/sdk/newsletter/useNewsletter'
 
 import Section from '../Section'
-import { useNewsletterQuery, useNewsletterQueryUpdate } from './NewsLetterQuery'
+import styles from './newsletter.module.scss'
 
 export interface NewsletterProps
   extends Omit<ComponentPropsWithRef<'form'>, 'title' | 'onSubmit'> {
@@ -12,51 +19,53 @@ export interface NewsletterProps
    */
   title: ReactNode
   /**
-   * A subtitle for the section.
+   * A description for the section.
    */
-  subtitle?: ReactNode
+  description?: ReactNode
+  /**
+   * The card Variant
+   */
+  card?: boolean
+  /**
+   * The compact version of the Newsletter component
+   */
+  lite?: boolean
 }
 
 const Newsletter = forwardRef<HTMLFormElement, NewsletterProps>(
-  function Newsletter({ title, subtitle, ...otherProps }, ref) {
-    const { subscribeUser, loading, data } = useNewsletterQuery()
-    const { updateUser } = useNewsletterQueryUpdate()
-    const [update, setUpdate] = useState(false)
-    const [message, setMessage] = useState('')
-    const [email, setEmail] = useState('')
-
+  function Newsletter(
+    { title, description, card = false, lite = false, ...otherProps },
+    ref
+  ) {
+    const { subscribeUser, loading, data } = useNewsletter()
+    const nameInputRef = useRef<HTMLInputElement>(null)
     const emailInputRef = useRef<HTMLInputElement>(null)
 
-    const clearMsg = (type = '') => {
-      if (type) {
-        setMessage(
-          type === 'error'
-            ? 'Erro ao tentar adicionar e-mail'
-            : 'E-mail inválido'
-        )
-      }
-
-      setTimeout(() => {
-        setMessage('')
-      }, 2000)
-    }
+    const { pushToast } = useUI()
 
     const handleSubmit = (event: FormEvent) => {
       event.preventDefault()
+      subscribeUser({
+        data: {
+          name: nameInputRef.current?.value ?? '',
+          email: emailInputRef.current?.value ?? '',
+        },
+      })
 
-      if (emailInputRef.current?.value) {
-        setEmail(emailInputRef.current?.value ?? '')
-        setMessage('Cadastrando e-mail....')
-        subscribeUser({ email: emailInputRef.current?.value })
-          .then(() => {
-            setUpdate(true)
-            clearMsg()
-          })
-          .catch(() => {
-            clearMsg('error')
-          })
+      if (data?.subscribeToNewsletter?.id) {
+        pushToast({
+          title: 'Hooray!',
+          message: 'Thank for your subscription.',
+          status: 'INFO',
+          icon: 'CircleWavyCheck',
+        })
       } else {
-        clearMsg('e-mail')
+        pushToast({
+          title: 'Oops.',
+          message: 'Something went wrong. Please Try again.',
+          status: 'ERROR',
+          icon: 'CircleWavyWarning',
+        })
       }
 
       const formElement = event.currentTarget as HTMLFormElement
@@ -64,53 +73,76 @@ const Newsletter = forwardRef<HTMLFormElement, NewsletterProps>(
       formElement.reset()
     }
 
-    if (update === true) {
-      const id = data?.newsLetter?.id ?? ''
-
-      setUpdate(false)
-      if (email) {
-        updateUser({ email, id })
-          .then(() => {
-            setMessage(id ? 'E-mail atualizado!' : 'E-mail cadastrado!')
-            clearMsg()
-          })
-          .catch(() => {
-            clearMsg('error')
-          })
-      } else {
-        clearMsg('e-mail')
-      }
-
-      setEmail('')
-    }
-
     return (
-      <Section data-fs-newsletter>
+      <Section
+        data-fs-newsletter={card ? 'card' : ''}
+        className={`layout__section ${styles.fsNewsletter}`}
+      >
         <Form
           data-fs-newsletter-form
           ref={ref}
           onSubmit={handleSubmit}
           {...otherProps}
+          className="layout__content"
         >
-          <div data-fs-newsletter-header>
-            {title}
-            {Boolean(subtitle) && subtitle}
-          </div>
+          <header data-fs-newsletter-header>
+            <h3>
+              <Icon name="Envelop" width={32} height={32} />
+              {title}
+            </h3>
+            {description && <span> {description}</span>}
+          </header>
 
           <div data-fs-newsletter-controls>
-            <Input
-              id="newsletter-email"
-              type="email"
-              name="newsletter-email"
-              placeholder="Email"
-              ref={emailInputRef}
-            />
-            <LoadingButton type="submit" loading={loading}>
-              Inscreva-se
-            </LoadingButton>
+            {lite ? (
+              <>
+                <InputText
+                  inputRef={emailInputRef}
+                  id="newsletter-email"
+                  label="Your Email"
+                  type="email"
+                  required
+                  actionable
+                  onSubmit={() => undefined}
+                  onClear={() => undefined}
+                  buttonActionText="Subscribe"
+                  displayClearButton={false}
+                />
+                <span data-fs-newsletter-addendum>
+                  By subscribing to our newsletter you agree to to our{' '}
+                  <Link href="/" inverse variant="inline">
+                    Privacy Policy.
+                  </Link>
+                </span>
+              </>
+            ) : (
+              <>
+                <InputText
+                  inputRef={nameInputRef}
+                  id="newsletter-name"
+                  label="Your Name"
+                  required
+                />
+                <InputText
+                  inputRef={emailInputRef}
+                  id="newsletter-email"
+                  label="Your Email"
+                  type="email"
+                  required
+                />
+                <span data-fs-newsletter-addendum>
+                  By subscribing to our newsletter you agree to to our{' '}
+                  <Link href="/" inverse variant="inline">
+                    Privacy Policy.
+                  </Link>
+                </span>
+                <Button variant="secondary" inverse type="submit">
+                  {loading ? 'Loading...' : 'Subscribe'}
+                </Button>
+              </>
+            )}
           </div>
         </Form>
-        <span data-fs-newsletter-message>{message}</span>
       </Section>
     )
   }
